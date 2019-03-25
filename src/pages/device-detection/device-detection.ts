17f77d531c278d1357757136c602e3c122780723
Hyemi Shin <hmshin@h3-ent.com>
2019-03-25 16:02:36 +0900

    기기 추가 컨펌알럿 및 로딩 추가

--- src/pages/device-detection/device-detection.ts
+++ src/pages/device-detection/device-detection.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {Storage} from '@ionic/storage';
-import {NavController, LoadingController} from "ionic-angular";
+import {NavController, LoadingController, AlertController} from "ionic-angular";
 
 import {DeviceDetectionService} from "../../services/device-detection-service";
 import {DevicesPage} from "../devices/devices";
@@ -15,7 +15,7 @@ export class DeviceDetectionPage {
   public detectedDevices: any;
   public numOfDevices: 0;
 
-  constructor(private storage: Storage, public nav: NavController, public deviceDetectionService: DeviceDetectionService, public loadingCtrl: LoadingController) {
+  constructor(private storage: Storage, public nav: NavController, public alertCtrl: AlertController, public deviceDetectionService: DeviceDetectionService, public loadingCtrl: LoadingController) {
     // set sample data
     // this.devices = deviceDetectionService.getAll();
 
@@ -29,25 +29,68 @@ export class DeviceDetectionPage {
 
   addDevice() {
       console.log('add device');
-      this.storage.set('detectedDevice', this.detectedDevices);
-      this.nav.push(DevicesPage);
+      this.showConfirm();
   }
 
   presentLoading(onDidDismissFunction) {
-        const loader = this.loadingCtrl.create({
-            content: "기기를 찾는중..."
-        });
+      const loader = this.loadingCtrl.create({
+          content: "기기를 찾는중..."
+      });
 
-        loader.onDidDismiss(() => {
-            console.log('Dismissed loading');
-            onDidDismissFunction();
-        });
+      loader.onDidDismiss(() => {
+          console.log('Dismissed loading');
+          onDidDismissFunction();
+      });
 
-        loader.present();
+      loader.present();
 
-        setTimeout(() => { //기기 감지 api 연동 필요
-            loader.dismiss();
-        }, 3000);
-    }
+      setTimeout(() => { //기기 감지 api 연동 필요
+          loader.dismiss();
+      }, 3000);
+  }
+
+  addDeviceLoading(onComplete) {
+      const loader = this.loadingCtrl.create({
+          content: "기기를 등록하는중입니다..."
+      });
+
+      loader.onDidDismiss(() => {
+          console.log('Dismissed loading');
+          onComplete();
+      });
+
+      loader.present();
+
+      setTimeout(() => { //기기 감지 api 연동 필요
+          loader.dismiss();
+      }, 3000);
+  }
+
+  showConfirm() {
+    const confirm = this.alertCtrl.create({
+      title: '기기 추가 확인',
+      message: '선택한 기기를 추가하시겠습니까?',
+      buttons: [
+        {
+          text: '취소',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: '확인',
+          handler: () => {
+            console.log('Agree clicked');
+            this.storage.set('detectedDevice', this.detectedDevices).then(()=>{
+              this.addDeviceLoading(()=>{
+                this.nav.push(DevicesPage);
+              });
+            });
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 
 }
